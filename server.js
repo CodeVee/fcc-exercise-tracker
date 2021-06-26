bb4da65ad1a0adfbf8b701dda9d2940e73e40fd3
Victor Onyebuchi <victorchike247@gmail.com>
2021-06-26 19:12:00 +0100

    added schema, model

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  log: [{
+    description: String,
+    duration: Number,
+    date: Date
+  }]
+});
+
+const User = mongoose.model("User", userSchema);
+
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
